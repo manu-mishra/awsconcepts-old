f55ae5f86b7fb34da73b7f0835f2189a91dafdb1
Mishra, Manu <mishra.manu@outlook.com>
2022-11-23 11:34:32 -0600

    few more warnings removed

--- apex/ui/src/App.tsx
+++ apex/ui/src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import { ApplicationRoutes } from './components/common/ApplicationRoutes'
-
-
-
-
-
 export const App: React.FunctionComponent = () => {
-  let greeting: string = 'waiting';
-  let InvokeApi = async () => {
-    console.log(API);
-    let resp = await API.get('api', '/values', {
-      responseType: 'json'
-    });
-    greeting = resp[0];
-    console.log(resp[0]);
-  }
   return (
     <Authenticator.Provider>
       <ApplicationRoutes />
